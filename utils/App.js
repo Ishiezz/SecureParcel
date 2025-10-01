fec0c123dd9ce93b077e8c9b34395f587bfe6fba
Ishiezz <singhishaa.24@gmail.com>
2025-10-01 15:49:48 +0530

    updated App.js and connected navigation in App.js

--- utils/App.js
+++ utils/App.js
@@ -1,16 +1,11 @@
 import * as React from 'react';
-import { Provider as PaperProvider, Button, Text } from 'react-native-paper';
-import { View } from 'react-native';
+import { Provider as PaperProvider} from 'react-native-paper';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
     <PaperProvider>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 24, marginBottom: 20 }}>SecureParcel 🚀</Text>
-        <Button mode="contained" onPress={() => console.log('Button Pressed')}>
-          Test Button
-        </Button>
-      </View>
+      <AppNavigator/>
     </PaperProvider>
   );
 }
